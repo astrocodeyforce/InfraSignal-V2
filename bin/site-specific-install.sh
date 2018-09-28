mysociety/fixmystreet

[Docker] Support Docker builds in install scripts
 - Adds support for additional variables intended to control when to
   install postfix and postgres.
 - Skips nginx setup and integration when performing a docker build.
 - Don't print usage during docker build

bin/site-specific-install.sh
@@ -26,14 +26,19 @@ misuse() {
 [ -z "$DISTRIBUTION" ] && misuse DISTRIBUTION
 [ -z "$VERSION" ] && misuse VERSION
 [ -z "$DEVELOPMENT_INSTALL" ] && misuse DEVELOPMENT_INSTALL
+[ -z "$DOCKER" ] && misuse DOCKER
+[ -z "$INSTALL_DB" ] && misue INSTALL_DB
+[ -z "$INSTALL_POSTFIX" ] && misuse INSTALL_POSTFIX
 
 add_locale cy_GB
 add_locale nb_NO
 add_locale de_CH
 
-install_postfix
+if [ $INSTALL_POSTFIX = true ]; then
+    install_postfix
+fi
 
-if [ ! "$DEVELOPMENT_INSTALL" = true ]; then
+if [ ! "$DEVELOPMENT_INSTALL" = true ] && [ ! "$DOCKER" = true ]; then
     install_nginx
     add_website_to_nginx
     # Check out the current released version
@@ -44,9 +49,11 @@ install_website_packages
 
 su -l -c "touch '$DIRECTORY/admin-htpasswd'" "$UNIX_USER"
 
-add_postgresql_user
+if [ $INSTALL_DB = true ]; then
+    add_postgresql_user
+fi
 
-export DEVELOPMENT_INSTALL
+export DEVELOPMENT_INSTALL DOCKER INSTALL_DB
 su -c "$REPOSITORY/bin/install-as-user '$UNIX_USER' '$HOST' '$DIRECTORY'" "$UNIX_USER"
 
 if [ ! "$DEVELOPMENT_INSTALL" = true ]; then
@@ -61,7 +68,7 @@ then
     overwrite_rc_local
 fi
 
-if [ ! "$DEVELOPMENT_INSTALL" = true ]; then
+if [ ! "$DEVELOPMENT_INSTALL" = true ] && [ ! "$DOCKER" = true ]; then
     # Tell the user what to do next:
 
     echo Installation complete - you should now be able to view the site at:

astrocodeyforce/InfraSignal-V2

bin/site-specific-install.sh
@@ -26,27 +26,41 @@ misuse() {
 [ -z "$DISTRIBUTION" ] && misuse DISTRIBUTION
 [ -z "$VERSION" ] && misuse VERSION
 [ -z "$DEVELOPMENT_INSTALL" ] && misuse DEVELOPMENT_INSTALL
+[ -z "$DOCKER" ] && misuse DOCKER
+[ -z "$INSTALL_DB" ] && misuse INSTALL_DB
+[ -z "$INSTALL_POSTFIX" ] && misuse INSTALL_POSTFIX
 
 add_locale cy_GB
 add_locale nb_NO
 add_locale de_CH
 
-install_postfix
+if [ $INSTALL_POSTFIX = true ]; then
+    install_postfix
+fi
 
 if [ ! "$DEVELOPMENT_INSTALL" = true ]; then
-    install_nginx
-    add_website_to_nginx
+    if [ ! "$DOCKER" = true ]; then
+      install_nginx
+      add_website_to_nginx
+    fi
     # Check out the current released version
     su -l -c "cd '$REPOSITORY' && git checkout '$VERSION' && git submodule update" "$UNIX_USER"
+fi
+
+# Create a log directoryfor Docker builds - this is normally done above.
+if [ $DOCKER = true ]; then
+    make_log_directory
 fi
 
 install_website_packages
 
 su -l -c "touch '$DIRECTORY/admin-htpasswd'" "$UNIX_USER"
 
-add_postgresql_user
+if [ $INSTALL_DB = true ]; then
+    add_postgresql_user
+fi
 
-export DEVELOPMENT_INSTALL
+export DEVELOPMENT_INSTALL DOCKER INSTALL_DB
 su -c "$REPOSITORY/bin/install-as-user '$UNIX_USER' '$HOST' '$DIRECTORY'" "$UNIX_USER"
 
 if [ ! "$DEVELOPMENT_INSTALL" = true ]; then
@@ -61,7 +75,7 @@ then
     overwrite_rc_local
 fi
 
-if [ ! "$DEVELOPMENT_INSTALL" = true ]; then
+if [ ! "$DEVELOPMENT_INSTALL" = true ] && [ ! "$DOCKER" = true ]; then
     # Tell the user what to do next:
 
     echo Installation complete - you should now be able to view the site at: