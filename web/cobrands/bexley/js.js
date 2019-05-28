mysociety/fixmystreet

[Bexley] Stop private property dead animal reporting.

web/cobrands/bexley/js.js
@@ -128,5 +128,27 @@ fixmystreet.assets.add(labeled_defaults, {
     asset_item: 'street light'
 });
 
+// We need to trigger the below function on subcategory change also
+$(function(){
+    $("#problem_form").on("change.category", "#form_DALocation", function() {
+        $(fixmystreet).trigger('report_new:category_change', [ $('#form_category') ]);
+    });
+});
+
+fixmystreet.message_controller.register_category({
+    body: defaults.body,
+    category: function() {
+        var cat = $('#form_category').val();
+        if (cat === 'Dead animal') {
+            var where = $('#form_DALocation').val();
+            if (where === 'Garden' || where === 'Other private property') {
+                return true;
+            }
+        }
+        return false;
+    },
+    message: 'Please follow the link above to pay to remove a dead animal from a private property.'
+});
+
 })();
 

astrocodeyforce/InfraSignal-V2

web/cobrands/bexley/js.js
@@ -130,5 +130,28 @@ fixmystreet.assets.add(labeled_defaults, {
     asset_item: 'street light'
 });
 
+// We need to trigger the below function on subcategory change also
+$(function(){
+    $("#problem_form").on("change.category", "#form_DALocation", function() {
+        $(fixmystreet).trigger('report_new:category_change', [ $('#form_category') ]);
+    });
+});
+
+fixmystreet.message_controller.register_category({
+    body: defaults.body,
+    category: function() {
+        var cat = $('#form_category').val();
+        if (cat === 'Dead animal') {
+            var where = $('#form_DALocation').val();
+            if (where === 'Garden' || where === 'Other private property') {
+                return true;
+            }
+        }
+        return false;
+    },
+    keep_category_extras: true,
+    message: 'Please follow the link below to pay to remove a dead animal from a private property.'
+});
+
 })();
 