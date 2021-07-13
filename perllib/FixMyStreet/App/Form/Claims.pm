mysociety/fixmystreet

[Bucks] no photo hashes in description.

perllib/FixMyStreet/App/Form/Claims.pm
@@ -1098,9 +1098,12 @@ sub format_for_display {
         }
     } elsif ( $field->{type} eq 'FileIdUpload' ) {
         if ( ref $value eq 'HASH' ) {
-           return join( ',', @{ $value->{filenames} } );
+           return join( ', ', @{ $value->{filenames} } );
         }
         return "";
+    } elsif ( $field->{type} eq 'FileIdPhoto' ) {
+        my $num = split /,/, $value;
+        return sprintf(mySociety::Locale::nget("%d photo", "%d photos", $num), $num);
     }
 
     return $value;

astrocodeyforce/InfraSignal-V2

perllib/FixMyStreet/App/Form/Claims.pm
@@ -1100,9 +1100,12 @@ sub format_for_display {
         }
     } elsif ( $field->{type} eq 'FileIdUpload' ) {
         if ( ref $value eq 'HASH' ) {
-           return join( ',', @{ $value->{filenames} } );
+           return join( ', ', @{ $value->{filenames} } );
         }
         return "";
+    } elsif ( $field->{type} eq 'Photo' ) {
+        my $num = split /,/, $value;
+        return sprintf(mySociety::Locale::nget("%d photo", "%d photos", $num), $num);
     }
 
     return $value;