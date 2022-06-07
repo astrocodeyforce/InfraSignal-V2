mysociety/fixmystreet

[Kingston] improve pending subscription checks
Because the kingston DD process redirects back to us after submission we
can set some metadata on the new report to indicate this. We can then
use this to check that the pending subscription got as far as submitting
the Direct Debit application, and only indicate there is a pending
subscription if this is the case.

t/app/controller/waste_kingston.t
@@ -625,10 +625,11 @@ FixMyStreet::override_config {
         is $new_report->title, 'Garden Subscription - New', 'correct title on report';
         is $new_report->get_extra_field_value('payment_method'), 'direct_debit', 'correct payment method on report';
         is $new_report->state, 'unconfirmed', 'report not confirmed';
+        is $new_report->get_extra_metadata('ddsubmitted'), undef, "direct debit not marked as submitted";
 
         $mech->get_ok('/waste/12345');
-        $mech->content_contains('You have a pending Garden Subscription');
-        $mech->content_lacks('Subscribe to Green Garden Waste');
+        $mech->content_lacks('You have a pending Garden Subscription');
+        $mech->content_contains('Subscribe to Green Garden Waste');
 
         $mech->get("/waste/dd_complete?customData=reference:$token^report_id:xxy");
         ok !$mech->res->is_success(), "want a bad response";
@@ -639,12 +640,17 @@ FixMyStreet::override_config {
         $mech->get_ok("/waste/dd_complete?customData=reference:$token^report_id:$report_id");
         $mech->content_contains('confirmation details once your Direct Debit');
 
+        $mech->get_ok('/waste/12345');
+        $mech->content_contains('You have a pending Garden Subscription');
+        $mech->content_lacks('Subscribe to Green Garden Waste');
+
         $mech->email_count_is( 1, "email sent for direct debit sub");
         my $email = $mech->get_email;
         my $body = $mech->get_text_body_from_email($email);
         like $body, qr/waste subscription/s, 'direct debit email confirmation looks correct';
         $new_report->discard_changes;
         is $new_report->state, 'unconfirmed', 'report still not confirmed';
+        is $new_report->get_extra_metadata('ddsubmitted'), 1, "direct debit marked as submitted";
         $new_report->delete;
     };
 
@@ -922,7 +928,7 @@ FixMyStreet::override_config {
         is $new_report->state, 'unconfirmed', 'report not confirmed';
 
         $mech->get_ok('/waste/12345');
-        $mech->content_contains('You have a pending Garden Subscription');
+        $mech->content_lacks('You have a pending Garden Subscription');
         $mech->content_lacks('Subscribe to Green Garden Waste');
 
         $mech->get("/waste/dd_complete?customData=reference:$token^report_id:xxy");
@@ -933,6 +939,10 @@ FixMyStreet::override_config {
         is $mech->res->code, 404, "got 404";
         $mech->get("/waste/dd_complete?customData=reference:$token^report_id:$report_id");
         $mech->content_contains('confirmation details once your Direct Debit');
+
+        $mech->get_ok('/waste/12345');
+        $mech->content_contains('You have a pending Garden Subscription');
+        $mech->content_lacks('Subscribe to Green Garden Waste');
 
         $new_report->discard_changes;
         is $new_report->state, 'unconfirmed', 'report still not confirmed';

astrocodeyforce/InfraSignal-V2

t/app/controller/waste_kingston.t
@@ -637,10 +637,11 @@ FixMyStreet::override_config {
         is $new_report->title, 'Garden Subscription - New', 'correct title on report';
         is $new_report->get_extra_field_value('payment_method'), 'direct_debit', 'correct payment method on report';
         is $new_report->state, 'unconfirmed', 'report not confirmed';
+        is $new_report->get_extra_metadata('ddsubmitted'), undef, "direct debit not marked as submitted";
 
         $mech->get_ok('/waste/12345');
-        $mech->content_contains('You have a pending garden subscription');
-        $mech->content_lacks('Subscribe to garden waste collection');
+        $mech->content_lacks('You have a pending garden subscription');
+        $mech->content_contains('Subscribe to garden waste collection');
 
         $mech->get("/waste/dd_complete?customData=reference:$token^report_id:xxy");
         ok !$mech->res->is_success(), "want a bad response";
@@ -651,6 +652,10 @@ FixMyStreet::override_config {
         $mech->get_ok("/waste/dd_complete?customData=reference:$token^report_id:$report_id");
         $mech->content_contains('confirmation details once your Direct Debit');
 
+        $mech->get_ok('/waste/12345');
+        $mech->content_contains('You have a pending garden subscription');
+        $mech->content_lacks('Subscribe to garden waste collection');
+
         $mech->email_count_is( 1, "email sent for direct debit sub");
         my $email = $mech->get_email;
         my $body = $mech->get_text_body_from_email($email);
@@ -658,6 +663,7 @@ FixMyStreet::override_config {
         like $body, qr/reference number is RBK-GGW-$report_id/, 'email has ID in it';
         $new_report->discard_changes;
         is $new_report->state, 'unconfirmed', 'report still not confirmed';
+        is $new_report->get_extra_metadata('ddsubmitted'), 1, "direct debit marked as submitted";
         $new_report->delete;
     };
 
@@ -935,7 +941,7 @@ FixMyStreet::override_config {
         is $new_report->state, 'unconfirmed', 'report not confirmed';
 
         $mech->get_ok('/waste/12345');
-        $mech->content_contains('You have a pending garden subscription');
+        $mech->content_lacks('You have a pending garden subscription');
         $mech->content_lacks('Subscribe to garden waste collection');
 
         $mech->get("/waste/dd_complete?customData=reference:$token^report_id:xxy");
@@ -946,6 +952,10 @@ FixMyStreet::override_config {
         is $mech->res->code, 404, "got 404";
         $mech->get("/waste/dd_complete?customData=reference:$token^report_id:$report_id");
         $mech->content_contains('confirmation details once your Direct Debit');
+
+        $mech->get_ok('/waste/12345');
+        $mech->content_contains('You have a pending garden subscription');
+        $mech->content_lacks('Subscribe to garden waste collection');
 
         $new_report->discard_changes;
         is $new_report->state, 'unconfirmed', 'report still not confirmed';