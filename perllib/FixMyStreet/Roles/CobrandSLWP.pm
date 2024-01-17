mysociety/fixmystreet

[SLWP] Add Cancelled Echo state.

perllib/FixMyStreet/Roles/CobrandSLWP.pm
@@ -278,6 +278,9 @@ sub waste_event_state_map {
             'Partially Completed' => 'closed',
             Rejected => 'closed',
         },
+        Cancelled => {
+            Cancelled => 'closed',
+        },
     };
 }
 

astrocodeyforce/InfraSignal-V2

perllib/FixMyStreet/Roles/CobrandSLWP.pm
@@ -243,6 +243,9 @@ sub waste_event_state_map {
             'Partially Completed' => 'closed',
             Rejected => 'closed',
         },
+        Cancelled => {
+            Cancelled => 'cancelled',
+        },
     };
 }
 