mysociety/fixmystreet

[Adelante] Add new payment integration.

perllib/Integrations/Adelante.pm
@@ -1,0 +1,116 @@
+package Integrations::Adelante;
+
+use Moo;
+with 'FixMyStreet::Roles::Syslog';
+
+use Crypt::Digest::SHA256;
+use DateTime;
+use HTTP::Request::Common;
+use JSON::MaybeXS;
+use LWP::UserAgent;
+
+has config => (
+    is => 'ro',
+    coerce => sub { return {} unless $_[0] },
+);
+
+has url => ( is => 'ro' );
+
+has log_ident => (
+    is => 'lazy',
+    default => sub { $_[0]->config->{log_ident}; },
+);
+
+sub pay {
+    my ($self, $args) = @_;
+
+    my $method = $args->{staff} ? 'PAYGUI' : 'PAY';
+
+    my @items;
+    foreach (@{$args->{items}}) {
+        push @items, {
+            Ref1 => $_->{cost_code},
+            Ref2 => $_->{reference},
+            #Narrative => $args->{uprn},
+            FundCode => $args->{fund_code},
+            Amount => $_->{amount},
+        };
+    }
+
+    my $obj = {
+        Function => $method,
+        Channel => $self->config->{channel},
+        PaymentReference => $args->{reference},
+        Name => $args->{name},
+        Address => $args->{address},
+        $args->{email} ? (EmailAddress => $args->{email}) : (),
+        $args->{phone} ? (Telephone => $args->{phone}) : (),
+        ReturnURL => $args->{returnUrl},
+        Lines => \@items,
+    };
+
+    my $resp = $self->call('Payment.ashx', $obj);
+    return $resp;
+}
+
+sub query {
+    my ($self, $args) = @_;
+
+    my $res = $self->call('Payment.ashx', {
+        Function => 'GET',
+        UID => $args->{reference},
+    });
+
+    # GET back includes PaymentID, UID, PaymentReference,
+    # Status: Authorised, Awaiting, Cancelled, Declined, Error, Expired, Pending
+    return $res;
+}
+
+sub echo {
+    my $self = shift;
+    my $resp = $self->call('Echo.ashx', {
+        Function => 'ECHO',
+        Input => 'Hello World',
+    });
+}
+
+sub call {
+    my ($self, $method, $data) = @_;
+    $self->log($method);
+    $self->log($data);
+
+    $data->{User} = $self->config->{username};
+    $data->{Password} = $self->config->{password};
+    $data = JSON::MaybeXS->new->utf8->encode($data);
+
+    my $sig = $self->sign($data . $self->config->{pre_shared_key});
+    my $url = $self->config->{url} . $method;
+    $url .= "?Format=JSON&Signature=" . $sig;
+
+    my $req = HTTP::Request::Common::POST(
+        $url,
+        Content_Type => "application/json",
+        Content => $data
+    );
+
+    my $ua = LWP::UserAgent->new;
+    my $response = $ua->request($req);
+    $response = $response->decoded_content;
+    $self->log($response || 'No response');
+
+    $response = JSON::MaybeXS->new->utf8->decode($response);
+    if ($response->{Result} ne 'OK') {
+        die $response->{Result};
+    }
+    return $response;
+}
+
+sub sign {
+    my ($self, $data) = @_;
+
+    my $hash = Crypt::Digest::SHA256->new;
+    $hash->add($data);
+    return $hash->b64digest;
+}
+
+1;

t/integrations/adelante.t
@@ -1,0 +1,91 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::MockModule;
+
+use_ok 'Integrations::Adelante';
+
+my %sent;
+
+my $lwp = Test::MockModule->new('LWP::UserAgent');
+$lwp->mock(request => sub {
+    my ($self, $req) = @_;
+
+    my ($function) = $req->content =~ /"Function":"(.*?)"/;
+    my $return = '"Result":"OK",';
+    if ($function eq 'PAY') {
+        like $req->content, qr/"Ref1":"CC"/;
+        like $req->content, qr/"Ref2":"reference"/;
+        like $req->content, qr/"Amount":1000/;
+        like $req->content, qr/"ReturnURL":"http:\/\/example\.org/;
+        $return .= '"UID":"UID", "Link":"https://example.org/"';
+    } elsif ($function eq 'GET') {
+        if ($req->content =~ /a-reference/) {
+            like $req->content, qr/"UID":"a-reference"/;
+            $return .= '"Status":"Authorised"';
+        } else {
+            like $req->content, qr/"UID":"a-staff-reference"/;
+            $return .= '"Status":"Authorised","MPOSID":"20013971","AuthCode":"999777"';
+        }
+    } elsif ($function eq 'ECHO') {
+        like $req->content, qr/"User":"username"/;
+        like $req->content, qr/"Input":"Hello World"/;
+        $return .= '"UID":"UID", "Link":"https://example.org/"';
+    } else {
+        is $function, 'ERROR';
+    }
+    return HTTP::Response->new(200, 'OK', [], "{$return}");
+});
+
+my $integration = Integrations::Adelante->new(
+    config => {
+        url => 'http://example.org/cc',
+        username => 'username',
+        password => 'password',
+        pre_shared_key => 'psk',
+        channel => 'CHANNEL',
+    }
+);
+
+subtest "check get redirect" => sub {
+    my $res = $integration->pay({
+        returnUrl => 'http://example.org/return',
+        fund_code => '32',
+        reference => 'reference',
+        name => 'name',
+        address => 'address',
+        email => 'email',
+        items => [ { cost_code => 'CC', reference => 'reference', amount => 1000 } ],
+    });
+
+    ok $res, 'got response';
+    is $res->{UID}, "UID";
+    is $res->{Link}, "https://example.org/";
+};
+
+subtest "check query" => sub {
+    my $res = $integration->query({
+        reference => 'a-reference',
+    });
+
+    ok $res, 'got response';
+    is $res->{Status}, "Authorised", 'transaction complete';
+};
+
+subtest "check staff query" => sub {
+    my $res = $integration->query({
+        reference => 'a-staff-reference',
+    });
+
+    ok $res, 'got response';
+    is $res->{Status}, "Authorised", 'transaction complete';
+    is $res->{MPOSID}, "20013971";
+    is $res->{AuthCode}, "999777";
+};
+
+subtest 'check echo' => sub {
+    my $res = $integration->echo();
+    is $res->{Result}, 'OK';
+};
+
+done_testing;

astrocodeyforce/InfraSignal-V2

perllib/Integrations/Adelante.pm
@@ -1,0 +1,117 @@
+package Integrations::Adelante;
+
+use Moo;
+with 'FixMyStreet::Roles::Syslog';
+
+use Crypt::Digest::SHA256;
+use DateTime;
+use HTTP::Request::Common;
+use JSON::MaybeXS;
+use LWP::UserAgent;
+
+has config => (
+    is => 'ro',
+    coerce => sub { return {} unless $_[0] },
+);
+
+has url => ( is => 'ro' );
+
+has log_ident => (
+    is => 'lazy',
+    default => sub { $_[0]->config->{log_ident}; },
+);
+
+sub pay {
+    my ($self, $args) = @_;
+
+    my $method = $args->{staff} ? 'PAY' : 'PAY3DS';
+
+    my @items;
+    foreach (@{$args->{items}}) {
+        push @items, {
+            Ref1 => $_->{cost_code},
+            Ref2 => $_->{reference},
+            #Narrative => $args->{uprn},
+            FundCode => $args->{fund_code},
+            Amount => $_->{amount},
+        };
+    }
+
+    my $obj = {
+        Function => $method,
+        Channel => $self->config->{channel},
+        $args->{staff} ? (MID => $self->config->{mid}) : (),
+        PaymentReference => $args->{reference},
+        Name => $args->{name},
+        Address => $args->{address},
+        $args->{email} ? (EmailAddress => $args->{email}) : (),
+        $args->{phone} ? (Telephone => $args->{phone}) : (),
+        ReturnURL => $args->{returnUrl},
+        Lines => \@items,
+    };
+
+    my $resp = $self->call('Payment.ashx', $obj);
+    return $resp;
+}
+
+sub query {
+    my ($self, $args) = @_;
+
+    my $res = $self->call('Payment.ashx', {
+        Function => 'GET',
+        UID => $args->{reference},
+    });
+
+    # GET back includes PaymentID, UID, PaymentReference,
+    # Status: Authorised, Awaiting, Cancelled, Declined, Error, Expired, Pending
+    return $res;
+}
+
+sub echo {
+    my $self = shift;
+    my $resp = $self->call('Echo.ashx', {
+        Function => 'ECHO',
+        Input => 'Hello World',
+    });
+}
+
+sub call {
+    my ($self, $method, $data) = @_;
+    $self->log($method);
+    $self->log($data);
+
+    $data->{User} = $self->config->{username};
+    $data->{Password} = $self->config->{password};
+    $data = JSON::MaybeXS->new->utf8->encode($data);
+
+    my $sig = $self->sign($data . $self->config->{pre_shared_key});
+    my $url = $self->config->{url} . $method;
+    $url .= "?Format=JSON&Signature=" . $sig;
+
+    my $req = HTTP::Request::Common::POST(
+        $url,
+        Content_Type => "application/json",
+        Content => $data
+    );
+
+    my $ua = LWP::UserAgent->new;
+    my $response = $ua->request($req);
+    $response = $response->decoded_content;
+    $self->log($response || 'No response');
+
+    $response = JSON::MaybeXS->new->utf8->decode($response);
+    if ($response->{Result} ne 'OK') {
+        die $response->{Result};
+    }
+    return $response;
+}
+
+sub sign {
+    my ($self, $data) = @_;
+
+    my $hash = Crypt::Digest::SHA256->new;
+    $hash->add($data);
+    return $hash->b64digest;
+}
+
+1;

t/integrations/adelante.t
@@ -1,0 +1,89 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::MockModule;
+
+use_ok 'Integrations::Adelante';
+
+my $lwp = Test::MockModule->new('LWP::UserAgent');
+$lwp->mock(request => sub {
+    my ($self, $req) = @_;
+
+    my ($function) = $req->content =~ /"Function":"(.*?)"/;
+    my $return = '"Result":"OK",';
+    if ($function eq 'PAY3DS') {
+        like $req->content, qr/"Ref1":"CC"/;
+        like $req->content, qr/"Ref2":"reference"/;
+        like $req->content, qr/"Amount":1000/;
+        like $req->content, qr/"ReturnURL":"http:\/\/example\.org/;
+        $return .= '"UID":"UID", "Link":"https://example.org/"';
+    } elsif ($function eq 'GET') {
+        if ($req->content =~ /a-reference/) {
+            like $req->content, qr/"UID":"a-reference"/;
+            $return .= '"Status":"Authorised"';
+        } else {
+            like $req->content, qr/"UID":"a-staff-reference"/;
+            $return .= '"Status":"Authorised","MPOSID":"20013971","AuthCode":"999777"';
+        }
+    } elsif ($function eq 'ECHO') {
+        like $req->content, qr/"User":"username"/;
+        like $req->content, qr/"Input":"Hello World"/;
+        $return .= '"UID":"UID", "Link":"https://example.org/"';
+    } else {
+        is $function, 'ERROR';
+    }
+    return HTTP::Response->new(200, 'OK', [], "{$return}");
+});
+
+my $integration = Integrations::Adelante->new(
+    config => {
+        url => 'http://example.org/cc',
+        username => 'username',
+        password => 'password',
+        pre_shared_key => 'psk',
+        channel => 'CHANNEL',
+    }
+);
+
+subtest "check get redirect" => sub {
+    my $res = $integration->pay({
+        returnUrl => 'http://example.org/return',
+        fund_code => '32',
+        reference => 'reference',
+        name => 'name',
+        address => 'address',
+        email => 'email',
+        items => [ { cost_code => 'CC', reference => 'reference', amount => 1000 } ],
+    });
+
+    ok $res, 'got response';
+    is $res->{UID}, "UID";
+    is $res->{Link}, "https://example.org/";
+};
+
+subtest "check query" => sub {
+    my $res = $integration->query({
+        reference => 'a-reference',
+    });
+
+    ok $res, 'got response';
+    is $res->{Status}, "Authorised", 'transaction complete';
+};
+
+subtest "check staff query" => sub {
+    my $res = $integration->query({
+        reference => 'a-staff-reference',
+    });
+
+    ok $res, 'got response';
+    is $res->{Status}, "Authorised", 'transaction complete';
+    is $res->{MPOSID}, "20013971";
+    is $res->{AuthCode}, "999777";
+};
+
+subtest 'check echo' => sub {
+    my $res = $integration->echo();
+    is $res->{Result}, 'OK';
+};
+
+done_testing;