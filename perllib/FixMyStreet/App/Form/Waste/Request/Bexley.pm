mysociety/fixmystreet

[Bexley][WW] Add reason selection for container request

perllib/FixMyStreet/App/Form/Waste/Request/Bexley.pm
@@ -24,4 +24,29 @@ has_field household_size => (
     ],
 );
 
+has_page request_reason => (
+    fields => ['request_reason', 'continue'],
+    title => 'Reason for request',
+    next => 'about_you',
+);
+
+has_field request_reason => (
+    type => 'Select',
+    widget => 'RadioGroup',
+    required => 1,
+    label => 'Why do you need new containers?',
+);
+
+sub options_request_reason {
+    my $form = shift;
+
+    my @options = (
+        'My existing bin is too small or big',
+        'My existing bin is damaged',
+        'My existing bin has gone missing',
+        'I have moved into a new development',
+    );
+    return map { { label => $_, value => $_ } } @options;
+}
+
 1;

astrocodeyforce/InfraSignal-V2

perllib/FixMyStreet/App/Form/Waste/Request/Bexley.pm
@@ -26,6 +26,33 @@ has_field household_size => (
     messages => { required => 'Please select an amount' },
 );
 
+has_page request_reason => (
+    fields => ['request_reason', 'continue'],
+    title => 'Reason for request',
+    next => 'about_you',
+);
+
+has_field request_reason => (
+    type => 'Select',
+    widget => 'RadioGroup',
+    required => 1,
+    label => 'Why do you need new bins?',
+    messages => { required => 'Please select a reason' },
+);
+
+sub options_request_reason {
+    my $form = shift;
+
+    my @options = (
+        'My existing bin is too small or big',
+        'My existing bin is damaged',
+        'My existing bin has gone missing',
+        'I have moved into a new development',
+        'Bins are no longer required',
+    );
+    return map { { label => $_, value => $_ } } @options;
+}
+
 sub validate {
     my $self = shift;
 