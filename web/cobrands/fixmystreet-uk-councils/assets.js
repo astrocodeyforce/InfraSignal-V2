mysociety/fixmystreet

[Glos] Store traffic signal information.

web/cobrands/fixmystreet-uk-councils/assets.js
@@ -762,6 +762,13 @@ fixmystreet.assets.gloucestershire.street_stylemap = new OpenLayers.StyleMap({
     })
 });
 
+fixmystreet.assets.gloucestershire.traffic_asset_details = function() {
+    var a = this.attributes;
+    return "Install No: " + a.install_no + "\n" +
+        "Equipment type: " + a.equip_typre + "\n" +
+        "LED/Halogen: " + a.led_halogen;
+};
+
 /* Hounslow */
 
 fixmystreet.assets.hounslow = {};

astrocodeyforce/InfraSignal-V2

web/cobrands/fixmystreet-uk-councils/assets.js
@@ -818,6 +818,13 @@ fixmystreet.assets.gloucestershire.street_stylemap = new OpenLayers.StyleMap({
     })
 });
 
+fixmystreet.assets.gloucestershire.traffic_asset_details = function() {
+    var a = this.attributes;
+    return "Install No: " + a.install_no + "\n" +
+        "Equipment type: " + a.equip_type + "\n" +
+        "LED/Halogen: " + a.led_halogen;
+};
+
 /* Hounslow */
 
 fixmystreet.assets.hounslow = {};