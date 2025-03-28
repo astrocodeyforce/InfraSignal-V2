mysociety/fixmystreet

[Waste] Image field support in enquiries.

perllib/FixMyStreet/App/Controller/Waste.pm
@@ -1099,6 +1099,20 @@ sub enquiry : Chained('property') : Args(0) {
             next;
         }
 
+        if ($_->{code} eq 'Image') {
+            push @$field_list, "extra_Image_fileid" => {
+                type => 'FileIdPhoto', num_photos_required => 0, linked_field => 'extra_Image',
+            };
+            push @$field_list, "extra_Image" => {
+                label => $_->{description},
+                type => 'Photo',
+                tags => {
+                    max_photos => 1,
+                },
+            };
+            next;
+        }
+
         my %config = (type => 'Text');
         my $datatype = $_->{datatype} || '';
         if ($datatype eq 'text') {

astrocodeyforce/InfraSignal-V2

perllib/FixMyStreet/App/Controller/Waste.pm
@@ -1125,6 +1125,20 @@ sub enquiry : Chained('property') : Args(0) {
             next;
         }
 
+        if ($_->{code} eq 'Image') {
+            push @$field_list, "location_photo_fileid" => {
+                type => 'FileIdPhoto', num_photos_required => 0, linked_field => 'location_photo',
+            };
+            push @$field_list, "location_photo" => {
+                label => $_->{description},
+                type => 'Photo',
+                tags => {
+                    max_photos => 1,
+                },
+            };
+            next;
+        }
+
         my %config = (type => 'Text');
         my $datatype = $_->{datatype} || '';
         if ($datatype eq 'text') {