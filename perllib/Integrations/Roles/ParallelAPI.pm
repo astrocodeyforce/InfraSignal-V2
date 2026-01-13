Mwebesanorman/fixmystreet

[WW] Check temp direcotory not a remnant
Bug with temp files being very occasionally left
behind - they are structured names to opearte across
two servers during a limited period.

But if they are left, they can be picked up again
at any time and hold stale data.

https://github.com/mysociety/societyworks/issues/4965

perllib/Integrations/Roles/ParallelAPI.pm
@@ -56,7 +56,11 @@ sub call_api {
 
     my $outdir = FixMyStreet->config('WASTEWORKS_BACKEND_TMP_DIR');
     mkdir($outdir) unless -d $outdir;
-    my $tmp = $outdir . "/" . md5_hex("$key $calls");
+    my $tmp_file_name = md5_hex("$key $calls");
+    if (-e $outdir . "/" . $tmp_file_name && _uncleared_dir($outdir . "/" . $tmp_file_name)) {
+        unlink($outdir . "/" . $tmp_file_name);
+    }
+    my $tmp = $outdir . "/" . $tmp_file_name;
 
     my @cmd = (
         FixMyStreet->path_to('bin/fixmystreet.com/call-wasteworks-backend'),
@@ -156,6 +160,23 @@ sub _parallel_api_calls {
     $pm->wait_all_children;
 
     return \%calls;
+}
+
+=head2 _uncleared_dir
+
+Return a positive if a directory is over a minute old
+
+=cut
+
+sub _uncleared_dir {
+    my $file = shift;
+
+    my $time = time;
+    my @stat stat($file);
+
+    if (($time - $stat[9]) > 60) {
+        return 1;
+    }
 }
 
 1;

astrocodeyforce/InfraSignal-V2

perllib/Integrations/Roles/ParallelAPI.pm
@@ -57,6 +57,9 @@ sub call_api {
     my $outdir = FixMyStreet->config('WASTEWORKS_BACKEND_TMP_DIR');
     mkdir($outdir) unless -d $outdir;
     my $tmp = $outdir . "/" . md5_hex("$key $calls");
+    if (-e $tmp && _uncleared_file($tmp)) {
+        unlink($tmp);
+    }
 
     my @cmd = (
         FixMyStreet->path_to('bin/fixmystreet.com/call-wasteworks-backend'),
@@ -73,7 +76,7 @@ sub call_api {
     my $start_call = 1;
 
     # uncoverable branch false
-    if (FixMyStreet->test_mode || $self->sample_data) {
+    if ((FixMyStreet->test_mode || $self->sample_data) && $self->sample_data != 2) {
         $start_call = 0;
         $data = $self->_parallel_api_calls(@_);
     } elsif (-e $tmp) {
@@ -96,7 +99,9 @@ sub call_api {
 
     # Either the temp file wasn't there, or it had bad data
     if ($start_call) {
-        if ($background) {
+        if ($self->sample_data == 2) {
+            $data = $self->_parallel_api_calls(@_);
+        } elsif ($background) {
             # wrap the $calls value in single quotes
             push(@cmd, "'" . pop(@cmd) . "'");
             # run it in the background
@@ -156,6 +161,23 @@ sub _parallel_api_calls {
     $pm->wait_all_children;
 
     return \%calls;
+}
+
+=head2 _uncleared_file
+
+Return a positive if a file is over a minute old
+
+=cut
+
+sub _uncleared_file {
+    my $file = shift;
+
+    my $time = time;
+    my @stat = stat($file);
+
+    if (($time - $stat[9]) > 60) {
+        return 1;
+    }
 }
 
 1;